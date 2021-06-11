3bdb46673310960a3ddaa143e404dd78b4dcce18
Vishal Chitrala <vishalchitrala@gmail.com>
2021-06-11 06:59:10 -0500

    Reviewed, pass commits.

---
+++ BikeStores/BikeStores/Customers.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [sales].[customers](
+	[customer_id] [int] IDENTITY(1,1) NOT NULL,
+	[first_name] [varchar](255) NOT NULL,
+	[last_name] [varchar](255) NOT NULL,
+	[phone] [varchar](25) NULL,
+	[email] [varchar](255) NOT NULL,
+	[street] [varchar](255) NULL,
+	[city] [varchar](50) NULL,
+	[state] [varchar](25) NULL,
+	[zip_code] [varchar](5) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[customer_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
