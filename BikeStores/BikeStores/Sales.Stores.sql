0c70bd2d6b847eec07b696f44a7b62fdcc076f91
vishalchitrala2020 <VishalChitrala@gmail.com>
2021-06-16 08:12:12 -0500

    Add new table Sales.Stores

---
+++ BikeStores/BikeStores/Sales.Stores.sql
@@ -0,0 +1,27 @@
+USE [BikeStores]
+GO
+
+/****** Object:  Table [sales].[stores]    Script Date: 6/16/2021 7:36:32 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [sales].[stores](
+	[store_id] [int] IDENTITY(1,1) NOT NULL,
+	[store_name] [varchar](255) NOT NULL,
+	[phone] [varchar](25) NULL,
+	[email] [varchar](255) NULL,
+	[street] [varchar](255) NULL,
+	[city] [varchar](255) NULL,
+	[state] [varchar](10) NULL,
+	[zip_code] [varchar](5) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[store_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
