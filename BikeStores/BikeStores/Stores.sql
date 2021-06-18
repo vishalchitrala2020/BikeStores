ec20028d1e85a4b04cb66d32eef919918630d67d
vishalchitrala2020 <VishalChitrala@gmail.com>
2021-06-18 07:42:34 -0500

    new table added

---
+++ BikeStores/BikeStores/Stores.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [sales].[stores](
+	[store_id] [int] IDENTITY(1,1) NOT NULL,
+	[store_name] [varchar](255) NOT NULL,
+	[phone] [varchar](25) NULL,
+	[email] [varchar](255) NULL,
+	[street] [varchar](255) NULL,
+	[city] [varchar](255) NULL,
+	[state] [varchar](10) NULL,
+	[zip_code] [varchar](5) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[store_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
